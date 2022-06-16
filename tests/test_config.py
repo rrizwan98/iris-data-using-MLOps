a241955fd03a9c31f153955baa8befb937925346
rrizwan98 <rrizwan1998@gmail.com>
2022-06-16 18:07:22 +0500

    git action workflow add

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,51 +9,30 @@ import yaml
 
 input_data = {
     "incorrect_range": 
-    {"fixed_acidity": 7897897, 
-    "volatile_acidity": 555, 
-    "citric_acid": 99, 
-    "residual_sugar": 99, 
-    "chlorides": 12, 
-    "free_sulfur_dioxide": 789, 
-    "total_sulfur_dioxide": 75, 
-    "density": 2, 
-    "pH": 33, 
-    "sulphates": 9, 
-    "alcohol": 9
+    {"SepalLengthCm": 7897897, 
+    "SepalWidthCm": 555, 
+    "PetalLengthCm": 99, 
+    "PetalWidthCm": 99
     },
 
     "correct_range":
-    {"fixed_acidity": 5, 
-    "volatile_acidity": 1, 
-    "citric_acid": 0.5, 
-    "residual_sugar": 10, 
-    "chlorides": 0.5, 
-    "free_sulfur_dioxide": 3, 
-    "total_sulfur_dioxide": 75, 
-    "density": 1, 
-    "pH": 3, 
-    "sulphates": 1, 
-    "alcohol": 9
+    {"SepalLengthCm": 4.4, 
+    "SepalWidthCm": 2.1, 
+    "PetalLengthCm": 1.1, 
+    "PetalWidthCm": 0.2, 
     },
 
     "incorrect_col":
-    {"fixed acidity": 5, 
-    "volatile acidity": 1, 
-    "citric acid": 0.5, 
-    "residual sugar": 10, 
-    "chlorides": 0.5, 
-    "free sulfur dioxide": 3, 
-    "total_sulfur dioxide": 75, 
-    "density": 1, 
-    "pH": 3, 
-    "sulphates": 1, 
-    "alcohol": 9
+    {"SepalLengthCm": 4.4, 
+    "SepalWidthCm": 2.1, 
+    "PetalLengthCm": 1.1, 
+    "PetalWidthCm": 0.2, 
     }
 }
 
 TARGET_range = {
-    "min": 3.0,
-    "max": 8.0
+    "min": 0.0,
+    "max": 2.0
 }
 
 def test_form_response_correct_range(data=input_data["correct_range"]):
